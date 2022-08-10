b3b6c933ef18eec08bd836e51e027a3e3b6412ca
Ezequiel Cuñado <ecunado@gmail.com>
2022-08-10 17:28:01 +0200

    feat: v2 (#84)
    Co-authored-by: ecunado <ecunado@handpoint.com>

---
+++ assets/js/d7544361.d7c051e0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[5330],{5824:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"javascript"}')}}]);
--- assets/js/e788dee7.4faacda9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[8647],{5126:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"android"}')}}]);
